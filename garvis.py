025c50e695554ab85190ed26d134c9f582382142
Gabe Skorepa <theguytrench@gmail.com>
2023-04-24 13:30:51 -0400

    Cleaning up the Garvis.py class
    Cleanup and moved functions into the class itself for smoother calls down the line for speech and adjusting the TTS.

--- garvis.py
+++ garvis.py
@@ -1,28 +1,29 @@
 from threading import Thread
 import pyttsx3
 
-ttsEngine = pyttsx3.init()
-
-
-def _ttsEngine():
-    voices = ttsEngine.getProperty('voices')
-    ttsEngine.setProperty('voice', voices[0].id)
-
-
-def speak(text, rate=150):
-    ttsEngine.setProperty('rate', rate)
-    ttsEngine.say(text)
-    ttsEngine.runAndWait()
-
 
 class Garvis(Thread):
-    _ttsEngine()
+    # INIT TTS ENGINE
+    ttsEngine = pyttsx3.init()
+
+    # TTS ENGINE VOICE SETTINGS
+    def _ttsVoice(self):
+        voices = self.ttsEngine.getProperty('voices')
+        self.ttsEngine.setProperty('voice', voices[0].id)
+
+    # TTS SPEECH COMMAND
+    def speak(self, text, rate=150):
+        self.ttsEngine.setProperty('rate', rate)
+        self.ttsEngine.say(text)
+        self.ttsEngine.runAndWait()
 
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
         self.start()
-        speak('Garvis Online')
+        self._ttsVoice()
+        print("Garvis Online.")
+        self.speak('Garvis Online')
 
 
 Garvis()
